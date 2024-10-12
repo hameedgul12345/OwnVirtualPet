d6e310cfd6ae1f363606dd9508a7019a2c15d788
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-12 21:57:49 +0500

    Making agePet function

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-var pet={
-    name:'mano',
-    type:'cat',
-    age:1,
-    happiness: 80,
-    hunger: 50,
-    fed:function(){
-        this.hunger=this.hunger-20;
-        
+var pet = {
+  name: "mano",
+  type: "cat",
+  age: 1,
+  happiness: 80,
+  hunger: 50,
+  fed: function () {
+    this.hunger = this.hunger - 20;
+    if (this.hunger < 0) {
+      this.hunger = 0;
     }
-}
+    alert(`${this.name} has been fed! Hunger level: ${this.hunger}`);
+  },
+  play: function () {
+    this.happiness += 20;
+    if (this.happiness > 100) {
+      this.happiness = 100;
+    }
+    this.hunger += 10; // Pet gets hungry after playing
+    alert(
+      `${this.name} is happy! Happiness level: ${this.happiness}. Hunger level: ${this.hunger}`
+    );
+  },
+  agePet: function () {
+    this.age += 1;
+    this.happiness -= 5;
+    this.hunger += 10;
+    alert(
+      `${this.name} has aged! Age: ${this.age}, Happiness: ${this.happiness}, Hunger: ${this.hunger}`
+    );
+  },
+};
 
-function playWithPet(){
-   pet.fed();
+function playWithPet() {
+  pet.fed();
 }
-playWithPet()
+playWithPet();
