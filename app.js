867b575a514d542616ab6216287e22516396ea29
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-12 20:34:34 +0500

    Making function

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ var pet={
     age:1,
     happiness: 80,
     hunger: 50,
+    fed:function(){
+        this.hunger=this.hunger-20;
+    }
 }
+
+function playWithPet(){
+    alert('meo')
+}
+playWithPet()
